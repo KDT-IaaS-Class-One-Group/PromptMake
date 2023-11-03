86345c2508e30d26a18d7f766598b7ad4ae4c206
kimhyun <forhn37@gmail.com>
2023-11-03 16:28:29 +0900

    html 구성 오류로 인해 재구성

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const router1 = express.Router();
 // 여기서 입력한 '/'는 서버에서 설정한 엔트리포인트뒤에 달려온다
 router1.get('/primary', function (req, res) {
-  fs.readFile("./data/primary.json",'utf-8', function(err, data) {
+  fs.readFile("index.html",'utf-8', function(err, data) {
     res.send(data);
   })
 })
