a73079559e7cc7bbffff902265dde640b4cb7ed7
kimhyun <forhn37@gmail.com>
2023-11-03 17:51:14 +0900

    서버 구성시 post로 데이터가 가나 그 데이터를 다루지 못하겠음

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
 const fs = require('fs')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static('public'));
+app.use(express.static('data'));
 
-// app.use('/static', express.static('public'));
-
-app.get('/', (req, res)=> {
-  fs.readFile('data/primary.json', (err,data) => {
-    res.send(data)
-  })
+app.get('/', (req, res) => {
+  res.send()
+})
+app.post('/add', (req, res) => {
+  const test = req.body
+  fs.writeFile(__dirname, test)
+  res.send(test)
 })
 
-app.listen(3000, ()=> {
+
+app.listen(3000, () => {
   console.log("http://localhost:3000/")
 })
--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,10 @@
-const express = require('express')
-const path = require('path')
-const fs = require('fs')
+// const express = require('express')
+// const path = require('path')
+// const fs = require('fs')
 
-const router1 = express.Router();
-// 여기서 입력한 '/'는 서버에서 설정한 엔트리포인트뒤에 달려온다
-router1.get('/primary', function (req, res) {
-  fs.readFile("index.html",'utf-8', function(err, data) {
-    res.send(data);
-  })
-})
-router1.get('/style', function (req, res) {
-  res.send();
-})
-module.exports = router1;
+// const router1 = express.Router();
+// // 여기서 입력한 '/'는 서버에서 설정한 엔트리포인트뒤에 달려온다
+
+// router1.get("/", (req,res)=> {
+//   res.send()
+// })
