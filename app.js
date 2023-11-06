9585e821fb45928999593558a2051f9e868fa464
kimhyun <forhn37@gmail.com>
2023-11-06 13:07:54 +0900

    form에 입력한 데이터를 설정하는 방법 이해

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(express.json());
 // 정적 파일 서빙 설정: 'public' 디렉토리의 파일을 정적 파일로 서빙
 app.use(express.static('public'));
 
+app.post('/postData', (req, res) => {
+  const data = req.body.data;
+  // 데이터를 사용하거나 처리할 로직 추가
+  console.log('POST 요청에 대한 데이터:', data);
+  res.send('POST 요청에 대한 응답: 데이터를 수정했습니다.');
+});
 // 서버 시작
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 실행 중입니다.`);
