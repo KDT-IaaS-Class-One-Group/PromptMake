10c4558b3cea33005cdaffd9b9495e73f5135ef3
kimhyun <forhn37@gmail.com>
2023-11-06 13:07:00 +0900

    post형식으로 사용하는 form을 좀 더 이해하는 중

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+
+// express.json() 미들웨어를 사용하여 JSON 파싱 활성화
+app.use(express.json());
+
+// 정적 파일 서빙 설정: 'public' 디렉토리의 파일을 정적 파일로 서빙
+app.use(express.static('public'));
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 실행 중입니다.`);
+});
+
