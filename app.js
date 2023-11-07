56d4f4f85342cc117ae61ded78c6fe1c900976e3
kimhyun <forhn37@gmail.com>
2023-11-07 11:58:28 +0900

    파일을 읽고 갖고오는데 문제 발생 , 어떤 절차가 있는지 잘 모르겠음

--- app.js
+++ app.js
@@ -1,60 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3002;
-const fs = require('fs');
-const path = require('path');
 
-app.use(express.json());
+const routes = require('./server/routes'); // 라우터 모듈 불러오기
+
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-
-// HTML 폼을 표시할 라우트
-app.get('/', (req, res) => {
-  // HTML 폼 파일을 클라이언트에 전송
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// POST 요청 처리
-app.post('/postData', (req, res) => {
-  const newData = req.body.data;
-  console.log(req.body)
-  const inputdataPath = path.join(__dirname, 'data', 'inputdata.json');
-
-  // 기존 데이터 불러오기
-  fs.readFile(inputdataPath, 'utf-8', (readErr, existingData) => {
-    if (readErr) {
-      // 파일 읽기 오류 처리
-      console.error('파일 읽기 오류:', readErr);
-      res.status(500).send('파일 읽기 오류 발생');
-    } else {
-      let inputDataArray = [];
-      try {
-        // 기존 데이터를 JSON 형식으로 파싱
-        inputDataArray = JSON.parse(existingData);
-      } catch (parseErr) {
-        // JSON 파싱 오류 처리
-        console.error('JSON 파싱 오류:', parseErr);
-        res.status(500).send('JSON 파싱 오류 발생');
-        return;
-      }
-
-      // 새 데이터를 기존 데이터에 추가
-      inputDataArray.push(newData);
-
-      // 데이터를 JSON 파일로 저장
-      fs.writeFile(inputdataPath, JSON.stringify(inputDataArray, null, 2), 'utf-8', (writeErr) => {
-        if (writeErr) {
-          // 파일 쓰기 오류 처리
-          console.error('파일 쓰기 오류:', writeErr);
-          res.status(500).send('파일 쓰기 오류 발생');
-        } else {
-          console.log('데이터가 성공적으로 파일에 추가되었습니다.');
-          res.send('데이터가 성공적으로 저장되었습니다.');
-        }
-      });
-    }
-  });
-});
+app.use(routes); // 라우터 사용
 
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 실행 중입니다.`);
--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,71 @@
-// const express = require('express')
-// const path = require('path')
-// const fs = require('fs')
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
-// const router1 = express.Router();
-// // 여기서 입력한 '/'는 서버에서 설정한 엔트리포인트뒤에 달려온다
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
-// router1.get("/", (req,res)=> {
-//   res.send()
-// })
+// HTML 폼을 표시할 라우트
+router.get('/', (req, res) => {
+  // HTML 폼 파일을 클라이언트에 전송
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+// POST 요청 처리
+router.post('/saveData', (req, res) => {
+  const newData = req.body.data;
+  const inputdataPath = path.join(__dirname,'..', 'data', 'inputdata.json');
+
+  fs.readFile(inputdataPath, 'utf-8', (readErr, existingData) => {
+    if (readErr) {
+      console.error('파일 읽기 오류:', readErr);
+      res.status(500).send('파일 읽기 오류 발생');
+    } else {
+      let inputDataArray = [];
+      try {
+        inputDataArray = JSON.parse(existingData);
+      } catch (parseErr) {
+        console.error('JSON 파싱 오류:', parseErr);
+        res.status(500).send('JSON 파싱 오류 발생');
+        return;
+      }
+
+      inputDataArray.push(newData);
+
+      fs.writeFile(inputdataPath, JSON.stringify(inputDataArray, null, 2), 'utf-8', (writeErr) => {
+        if (writeErr) {
+          console.error('파일 쓰기 오류:', writeErr);
+          res.status(500).send('파일 쓰기 오류 발생');
+        } else {
+          console.log('데이터가 성공적으로 파일에 추가되었습니다.');
+          res.json({ message: '데이터가 성공적으로 저장되었습니다.' });
+        }
+      });
+    }
+  });
+});
+
+router.get('/getData', (req, res) => {
+  const inputdataPath = path.join(__dirname,'..', 'data', 'inputdata.json');
+
+  fs.readFile(inputdataPath, 'utf-8', (readErr, jsonData) => {
+    if (readErr) {
+      console.error('파일 읽기 오류:', readErr);
+      res.status(500).send('파일 읽기 오류 발생');
+    } else {
+      const data = JSON.parse(jsonData);
+    }
+  });
+
+  fs.writeFile(inputdataPath, JSON.stringify(inputDataArray, null, 2), 'utf-8', (writeErr) => {
+    if (writeErr) {
+      console.error('파일 쓰기 오류:', writeErr);
+      res.status(500).send('파일 쓰기 오류 발생');
+    } else {
+      console.log('데이터가 성공적으로 파일에 추가되었습니다.');
+      res.json({ message: '데이터가 성공적으로 저장되었습니다.' });
+    }
+  });
+});
+
+module.exports = router;
