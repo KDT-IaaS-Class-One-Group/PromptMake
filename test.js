3d265a8a260ce2abb55e7fbe429d0bd1c7cea18e
kimhyun <forhn37@gmail.com>
2023-11-07 10:10:18 +0900

    postForm처리시 get방식으로 데이터가 전달되는데 어디에 전달되는지 파악중

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-const myArray = [1, 2, 3, 4, 5];
+// const myArray = [1, 2, 3, 4, 5];
 
 // for (const num of myArray) {
 //     console.log(num); // 배열 요소를 출력
 // }
-for(let i=0; i<myArray.length; i++) {
-  const number = myArray[i]
-  console.log(number)
-}
+// for(let i=0; i<myArray.length; i++) {
+//   const number = myArray[i]
+//   console.log(number)
+// }
 
-const yaho = {
-  username: "john_doe",
-  email: "john@example.com"
-}
-const yoohoo = [
-  ["username", "john_doe"],
-  ["email", "john@example.com"]
-]
-for(const pair of yoohoo) {
-  console.log(pair[0]+ ' : '+ pair[1])
-}
+// const yaho = {
+//   username: "john_doe",
+//   email: "john@example.com"
+// }
+// const yoohoo = [
+//   ["username", "john_doe"],
+//   ["email", "john@example.com"]
+// ]
+// for(const pair of yoohoo) {
+//   console.log(pair[0]+ ' : '+ pair[1])
+// }
