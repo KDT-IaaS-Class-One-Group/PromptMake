3d909859da1014d6f3bc13927aa5eed33801cac9
kimhyun <forhn37@gmail.com>
2023-11-07 18:33:14 +0900

    입력 데이터 외 답변데이터도 출력 예정

--- public/hamburger.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-
-export const hamburgerclicker = hamburger.addEventListener("click", function() {
-    menu.classList.toggle("show-menu");
-});
