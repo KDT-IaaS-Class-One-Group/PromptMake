869bf4b2589f011c25a1eb9a14c23397775a8b0a
kimhyun <forhn37@gmail.com>
2023-11-08 18:15:39 +0900

    다크, 데이 모듈화문제 다시 해봐야징

--- public/changebg.js
+++ public/changebg.js
@@ -1,23 +1,20 @@
-const darkmode = document.getElementById('darkmode');
-const daymode = document.getElementById('daymode');
-const RightBox = document.getElementById('RightBox');
-
-const root = document.getElementById("root");
-const hamburgerproperty = document.getElementById('hamburgerproperty');
-const userInfo = document.getElementById('userInfo')
 
 
-// 다크모드 모듈작성
-export const darkmodechanger = darkmode.addEventListener('click', function (bgcolor, fontcolor) {
-  root.style.backgroundColor = bgcolor;
-  RightBox.style.color = fontcolor;
-  hamburgerproperty.style.color = fontcolor;
-  userInfo.style.color = fontcolor
-});
-// 데이모드 모듈 작성
-export const daymodechanger = daymode.addEventListener('click', function (bgcolor, fontcolor) {
-  root.style.backgroundColor = bgcolor;
-  RightBox.style.color = fontcolor;
-  hamburgerproperty.style.color = fontcolor;
-  userInfo.style.color = fontcolor
-});
+
+// // 다크모드 모듈작성
+// export const darkmodechanger = darkmode.addEventListener('click', function (bgcolor, fontcolor) {
+//   root.style.backgroundColor = bgcolor;
+//   RightBox.style.color = fontcolor;
+//   hamburgerproperty.style.color = fontcolor;
+//   userInfo.style.color = fontcolor
+// });
+// // 데이모드 모듈 작성
+// export const daymodechanger = daymode.addEventListener('click', function (bgcolor, fontcolor) {
+//   root.style.backgroundColor = bgcolor;
+//   RightBox.style.color = fontcolor;
+//   hamburgerproperty.style.color = fontcolor;
+//   userInfo.style.color = fontcolor
+// });
+
+
+// 모듈화 실패 다시 해보기
